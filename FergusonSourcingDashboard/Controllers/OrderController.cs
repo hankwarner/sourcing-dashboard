0b9b78847bfb377781b95a73b64a73f1f3982b54
Hank Warner <h.warner@nbsupply.com>
2020-11-05 18:18:20 -0500

    first commit

---
+++ FergusonSourcingDashboard/Controllers/OrderController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FergusonSourcingCore.Models;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace FergusonSourcingDashboard.Controllers
+{
+    public class OrderController
+    {
+        
+        public static Document GetManualOrderById(DocumentClient cosmosClient, string id)
+        {
+            var manualOrdersContainerName = Environment.GetEnvironmentVariable("MANUAL_ORDERS_CONTAINER_NAME");
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("sourcing-engine", manualOrdersContainerName);
+            var feedOption = new FeedOptions { EnableCrossPartitionQuery = true };
+
+            var query = new SqlQuerySpec
+            {
+                QueryText = "SELECT VALUE o FROM o WHERE o.id = @id",
+                Parameters = new SqlParameterCollection()
+                {
+                    new SqlParameter("@id", id)
+                }
+            };
+
+            return cosmosClient.CreateDocumentQuery<Document>(collectionUri, query, feedOption)
+                .AsEnumerable().FirstOrDefault();
+        }
+
+
+        public async static Task UnclaimOrders(List<Document> manualOrderDocs, DocumentClient document)
+        {
+            manualOrderDocs.ForEach(async orderDoc =>
+            {
+                ManualOrder manualOrder = (dynamic)orderDoc;
+                manualOrder.claimed = false;
+
+                await document.ReplaceDocumentAsync(orderDoc.SelfLink, manualOrder);
+            });
+        }
+
+
+
+        /// <summary>
+        ///     This function provides a safety net for missing unit prices and extended prices on manual orders. If any item on the manual
+        ///     order does not have a unit price or extended price, gets the prices from the matching ATG Order and writes them on the manual order.
+        /// </summary>
+        /// <param name="document">CosmosDB document client for CRUD operations.</param>
+        /// <param name="manualOrderDocs">Manual order documents that are incomplete and unclaimed.</param>
+        /// <returns>List of manual orders that need to be worked by a rep.</returns>
+        public static async Task<List<ManualOrder>> SetItemFields(DocumentClient document, IEnumerable<Document> manualOrderDocs)
+        {
+            var manualOrders = new List<ManualOrder>();
+
+            foreach (var orderDoc in manualOrderDocs)
+            {
+                ManualOrder manualOrder = (dynamic)orderDoc;
+
+                // if unit price is missing on the first item, it is missing from all items
+                var unitPrice = manualOrder.sourcing[0].items[0].unitPrice;
+                var prefShipVia = manualOrder.sourcing[0].items[0].preferredShipVia;
+                // Temporaryily removing this to see if phone will set on its own
+                //var phone = manualOrder.paymentOnAccount.payment.phone;
+                //var needsUpdate = string.IsNullOrEmpty(unitPrice) || string.IsNullOrEmpty(phone);
+
+                var needsUpdate = string.IsNullOrEmpty(unitPrice) || string.IsNullOrEmpty(prefShipVia);
+
+                if (needsUpdate)
+                {
+                    var ordersContainerName = Environment.GetEnvironmentVariable("ORDERS_CONTAINER_NAME");
+                    var collectionUri = UriFactory.CreateDocumentCollectionUri("sourcing-engine", ordersContainerName);
+                    var feedOption = new FeedOptions { EnableCrossPartitionQuery = true };
+
+                    var query = new SqlQuerySpec
+                    {
+                        QueryText = "SELECT * FROM c WHERE c.id = @id",
+                        Parameters = new SqlParameterCollection() { new SqlParameter("@id", manualOrder.id) }
+                    };
+
+                    var order = document.CreateDocumentQuery<AtgOrderRes>(collectionUri, query, feedOption)
+                        .AsEnumerable().FirstOrDefault();
+
+                    if(order != null)
+                    {
+                        // Set phone number
+                        // Temporaryily removing this to see if it will set on its own
+                        //manualOrder.paymentOnAccount.payment.phone = order.paymentOnAccount?.First().phone;
+
+                        // Set unit prices and extended prices
+                        manualOrder.sourcing.ForEach(source =>
+                        {
+                            source.items.ForEach(item =>
+                            {
+                                var atgLine = order.items.Where(l => l.lineId == item.lineItemId).Select(l => l).FirstOrDefault();
+
+                                // Write unit price and extended price to ManualOrder
+                                item.unitPrice = atgLine.unitPrice;
+                                item.extendedPrice = atgLine.extendedPrice;
+                                item.preferredShipVia = atgLine.preferredShipVia;
+                            });
+                        });
+
+                        await document.ReplaceDocumentAsync(orderDoc.SelfLink, manualOrder);
+                    }
+                }
+
+                manualOrders.Add(manualOrder);
+            }
+
+            return manualOrders;
+        }
+    }
+}
---
+++ FergusonSourcingDashboard/SourcingDashboard.cs
@@ -0,0 +1,255 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
+using Microsoft.Azure.Documents;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using FergusonSourcingCore.Models;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using FergusonSourcingDashboard.Controllers;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FergusonSourcingDashboard
+{
+    public class SourcingDashboard
+    {
+        public static IConfiguration _config { get; set; }
+
+        public SourcingDashboard(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [FunctionName("GetManualOrders")]
+        public static async Task<IActionResult> GetManualOrders(
+                    [HttpTrigger(AuthorizationLevel.Function, "get", Route = "manual-orders")] HttpRequest req,
+                    [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient document,
+                    ILogger log)
+        {
+            var manualOrdersContainerName = Environment.GetEnvironmentVariable("MANUAL_ORDERS_CONTAINER_NAME");
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("sourcing-engine", manualOrdersContainerName);
+            var feedOption = new FeedOptions { EnableCrossPartitionQuery = true };
+
+            var query = "SELECT VALUE o FROM o WHERE o.orderComplete = false AND o.claimed = false ORDER BY o.orderSubmitDate ASC";
+
+            var distinctDocuments = document.CreateDocumentQuery<Document>(collectionUri, query, feedOption)
+                .ToList().Distinct();
+#if DEBUG
+            // Limit to last 50 orders
+            distinctDocuments = distinctDocuments.Skip(Math.Max(0, distinctDocuments.Count() - 50)).Take(50);
+#endif
+            var manualOrders = await OrderController.SetItemFields(document, distinctDocuments);
+
+            return new OkObjectResult(manualOrders);
+        }
+
+
+        [FunctionName("GetManualOrder")]
+        public static IActionResult GetManualOrder(
+                    [HttpTrigger(AuthorizationLevel.Function, "get", Route = "manual-orders/{id}")] HttpRequest req,
+                    [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient manualOrders,
+                    ILogger log,
+                    string id)
+        {
+            var manualOrdersContainerName = Environment.GetEnvironmentVariable("MANUAL_ORDERS_CONTAINER_NAME");
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("sourcing-engine", manualOrdersContainerName);
+            var feedOption = new FeedOptions { EnableCrossPartitionQuery = true };
+
+            var query = new SqlQuerySpec
+            {
+                QueryText = "SELECT * FROM c WHERE c.id = @id",
+                Parameters = new SqlParameterCollection() { new SqlParameter("@id", id) }
+            };
+
+            var manualOrder = manualOrders.CreateDocumentQuery<ManualOrder>(collectionUri, query, feedOption)
+                .AsEnumerable().FirstOrDefault();
+
+            if(manualOrder == null)
+            {
+                log.LogError("Order ID not found");
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(manualOrder);
+        }
+
+
+        [FunctionName("UpdateManualOrderNote")]
+        public static async Task<IActionResult> UpdateManualOrderNote(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "order/{id}/note"), RequestBodyType(typeof(OrderNote), "order note")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient cosmosClient,
+            ILogger log,
+            string id)
+        {
+            var manualOrdersContainerName = Environment.GetEnvironmentVariable("MANUAL_ORDERS_CONTAINER_NAME");
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("sourcing-engine", manualOrdersContainerName);
+            var feedOption = new FeedOptions { EnableCrossPartitionQuery = true };
+
+            var jsonReq = new StreamReader(req.Body).ReadToEnd();
+            var reqBody = JsonConvert.DeserializeObject<OrderNote>(jsonReq);
+            var note = reqBody?.note;
+
+            if (reqBody == null || note == null)
+            {
+                log.LogError("Request did not contain a JSON body with note property");
+                return new NotFoundResult();
+            }
+
+            log.LogInformation($"Order ID: {id}");
+            log.LogInformation(@"Request: {jsonReq}", jsonReq);
+
+            var query = new SqlQuerySpec
+            {
+                QueryText = "SELECT * FROM c WHERE c.id = @id",
+                Parameters = new SqlParameterCollection() { new SqlParameter("@id", id) }
+            };
+
+            var manualOrderDoc = cosmosClient.CreateDocumentQuery<Document>(collectionUri, query, feedOption)
+                .AsEnumerable().FirstOrDefault();
+
+            if (manualOrderDoc == null)
+            {
+                log.LogError("Order ID not found");
+                return new NotFoundResult();
+            }
+
+            ManualOrder manualOrder = (dynamic)manualOrderDoc;
+            manualOrder.notes = note;
+
+            log.LogInformation(@"Manual order: {ManualOrder}", manualOrder);
+            await cosmosClient.ReplaceDocumentAsync(manualOrderDoc.SelfLink, manualOrder);
+
+            return new OkObjectResult(manualOrder);
+        }
+
+
+        [FunctionName("ClaimOrder")]
+        public static async Task<IActionResult> ClaimOrder(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "order/claim/{id}")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient cosmosClient,
+            ILogger log,
+            string id)
+        {
+            var updatedOrderData = OrderController.GetManualOrderById(cosmosClient, id);
+
+            if (updatedOrderData == null)
+            {
+                return new NotFoundResult();
+            }
+
+            ManualOrder updatedOrder = (dynamic)updatedOrderData;
+
+            updatedOrder.claimed = true;
+
+            await cosmosClient.ReplaceDocumentAsync(updatedOrderData.SelfLink, updatedOrder);
+
+            return new OkObjectResult(updatedOrder);
+        }
+
+
+        [FunctionName("IsOrderClaimed")]
+        public static async Task<IActionResult> IsOrderClaimed(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "order/is-claimed/{id}")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient cosmosClient,
+            ILogger log,
+            string id)
+        {
+            var updatedOrderData = OrderController.GetManualOrderById(cosmosClient, id);
+
+            if (updatedOrderData == null)
+            {
+                return new NotFoundResult();
+            }
+
+            ManualOrder order = (dynamic)updatedOrderData;
+
+            return new OkObjectResult(order.claimed || order.orderComplete);
+        }
+
+
+        [FunctionName("ReleaseOrder")]
+        public static async Task<IActionResult> ReleaseOrder(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "order/release/{id}")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient cosmosClient,
+            ILogger log,
+            string id)
+        {
+            var updatedOrderData = OrderController.GetManualOrderById(cosmosClient, id);
+
+            if (updatedOrderData == null)
+            {
+                return new NotFoundResult();
+            }
+
+            ManualOrder updatedOrder = (dynamic)updatedOrderData;
+
+            updatedOrder.claimed = false;
+
+            await cosmosClient.ReplaceDocumentAsync(updatedOrderData.SelfLink, updatedOrder);
+
+            return new OkObjectResult(updatedOrder);
+        }
+
+
+        [FunctionName("CompleteOrder")]
+        public static async Task<IActionResult> CompleteOrder(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "order/complete/{id}")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient cosmosClient,
+            ILogger log,
+            string id)
+        {
+            var updatedOrderData = OrderController.GetManualOrderById(cosmosClient, id);
+
+            if (updatedOrderData == null)
+            {
+                return new NotFoundResult();
+            }
+
+            ManualOrder updatedOrder = (dynamic)updatedOrderData;
+
+            updatedOrder.orderComplete = true;
+
+            await cosmosClient.ReplaceDocumentAsync(updatedOrderData.SelfLink, updatedOrder);
+
+            return new OkObjectResult(updatedOrder);
+        }
+
+
+
+        /// <summary>
+        ///     Unclaims manual orders that are claimed not marked completed in the web app. Runs nightly Mon-Fri.
+        /// </summary>
+        /// <returns></returns>
+        [FunctionName("ResetClaimedIncompleteOrders")]
+        public async Task ResetClaimedIncompleteOrders(
+                    [TimerTrigger("0 0 23 * * 1-5")] TimerInfo timer, // every workday at 11pm
+                    [CosmosDB(ConnectionStringSetting = "AzureCosmosDBConnectionString"), SwaggerIgnore] DocumentClient document,
+                    ILogger log)
+        {
+            var manualOrdersContainerName = Environment.GetEnvironmentVariable("MANUAL_ORDERS_CONTAINER_NAME");
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("sourcing-engine", manualOrdersContainerName);
+            var feedOption = new FeedOptions { EnableCrossPartitionQuery = true };
+
+            var query = "SELECT VALUE o FROM o WHERE o.orderComplete = false AND o.claimed = true ORDER BY o.id";
+
+            var claimedIncompleteOrderDocs = document.CreateDocumentQuery<Document>(collectionUri, query, feedOption)
+                .AsEnumerable().Distinct();
+            log.LogInformation(@"Claimed, Incomplete Orders: {ClaimedIncompleteOrderDocs}", claimedIncompleteOrderDocs);
+
+            if (claimedIncompleteOrderDocs.Count() == 0)
+            {
+                log.LogInformation("No claimed uncompleted orders found.");
+                return;
+            }
+
+            await OrderController.UnclaimOrders(claimedIncompleteOrderDocs.ToList(), document);
+        }
+    }
+}
